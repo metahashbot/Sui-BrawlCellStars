e3ccb41afa3e2e30085c0766a73b9ebb57e07009
ypcy <139439561+who1know1me@users.noreply.github.com>
2025-05-16 21:42:23 +0800

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import BettingPage from './pages/BettingPage';
 import MarketplacePage from './pages/MarketplacePage';
 import NotFoundPage from './pages/NotFoundPage';
 import { GameProvider } from './context/GameContext';
-import { WalletProvider } from './context/WalletContext';
 import { BettingProvider } from './context/BettingContext';
 
 function App() {
   return (
-    <Router>
-      <WalletProvider>
         <GameProvider>
           <BettingProvider>
             <div className="min-h-screen bg-gray-900 text-white">
@@ -26,8 +22,6 @@ function App() {
             </div>
           </BettingProvider>
         </GameProvider>
-      </WalletProvider>
-    </Router>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { networkConfig } from './config/networkConfig.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider defaultNetwork="testnet" networks={networkConfig}>
         <WalletProvider autoConnect>
-          <App />
+          <BrowserRouter basename="/sui-battle-arena">
+            <App />
+          </BrowserRouter>
         </WalletProvider>
       </SuiClientProvider>
     </QueryClientProvider>

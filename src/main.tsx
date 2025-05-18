5d2e171339d099ae9ac0afffc244cd15624a2b77
ypcy <139439561+who1know1me@users.noreply.github.com>
2025-05-18 12:14:12 +0800

    update provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,27 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { SuiClientProvider, WalletProvider as DappKitWalletProvider } from "@mysten/dapp-kit"; // 重命名以区分
 import { networkConfig } from './config/networkConfig.tsx'
 import { BrowserRouter } from 'react-router-dom';
+import { WalletProvider as AppWalletProvider } from './context/WalletContext';
+
 
 const queryClient = new QueryClient();
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <SuiClientProvider defaultNetwork="testnet" networks={networkConfig}>
-        <WalletProvider autoConnect>
-          <BrowserRouter basename="/sui-battle-arena">
-            <App />
-          </BrowserRouter>
-        </WalletProvider>
+        <DappKitWalletProvider autoConnect> 
+          <AppWalletProvider> 
+            <BrowserRouter basename="/sui-battle-arena">
+              <App />
+            </BrowserRouter>
+          </AppWalletProvider>
+        </DappKitWalletProvider>
       </SuiClientProvider>
     </QueryClientProvider>
   </StrictMode>
